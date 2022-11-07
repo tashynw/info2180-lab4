454183e11f412d4594170cfa153a1d79577e3771
unknown <wallacetashyn@gmail.com>
2022-11-06 19:25:06 -0500

    exercise 2 completed-fix (used js fetch api instead)

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 window.onload = function() {
     $("#search-button").click(function(){
-        $.ajax({
-            contentType: 'application/json',
-            dataType: 'json',
-            url: 'http://localhost/info2180-lab4/superheroes.php',
-            method: 'GET',
-            crossDom,
-            success: function (data) {
-                alert(data);
-            },
-            error: function () {
-                console.log("Request Failed");
-            }
-        });
+        fetch("superheroes.php")
+            .then((response)=>response.text())
+            .then((response)=>alert(response))
     })
 }
